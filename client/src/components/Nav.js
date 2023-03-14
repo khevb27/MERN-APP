f04aab4693e43d8e3989b8e5b5f08f8570e7298c
kyle <kylehippledev@gmail.com>
2023-03-14 13:14:24 -0500

    Buttons

---
+++ client/src/components/Nav.js
@@ -0,0 +1,24 @@
+import Nav from 'react-bootstrap/Nav';
+import "../App.css"
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+function NavBar() {
+return (
+    <Navbar bg="dark" expand="lg" className="justify-content-center"> {/* Make the navbar inline */}
+    <Container>
+      <Navbar.Brand href="/">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="navbar-nav">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="#">Sign Up</Nav.Link>
+          <Nav.Link href="#">My Trips</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+    );
+}
+
+export default NavBar;
