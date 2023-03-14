84fd1a2ad4b9899be3bc2eada6635dad02f0626c
kyle <kylehippledev@gmail.com>
2023-03-14 12:51:55 -0500

    Routes and Browser

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -10,7 +10,7 @@ function HomePage() {
       </header>
       <Navbar bg="dark" expand="lg" className="justify-content-center"> {/* Make the navbar inline */}
         <Container>
-          <Navbar.Brand href="#">Home</Navbar.Brand>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="navbar-nav">
